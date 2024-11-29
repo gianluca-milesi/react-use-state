1aa731526f92447288ffc9f81fca06953024c3a9
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-29 16:18:30 +0100

    Starting Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./layouts/Main.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,8 @@
+function Main(){
+
+    return(
+        <div>main</div>
+    )
+}
+
+export default Main
