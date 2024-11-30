3a8d8e591ba2ae7534d32970a2593ffaee0d4eee
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-30 23:58:09 +0100

    ADD: host script. Refactoring button colors.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import style from "./Button.module.css"
 function Button({ title, onClick, isActive }) {
 
     return (
-        <button className= {`${style.button} ${isActive ? style.active : ""}`} onClick={onClick}>{title}</button>
+        <button className={`${style.button} ${isActive ? style.bg_gold : style.bg_azure}`} onClick={onClick}>{title}</button>
     )
 }
 
